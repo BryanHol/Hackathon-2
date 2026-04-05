109d67b623a4999bd4ac4488bfb874b31face44d
Anooneee <72642342+Anooneee@users.noreply.github.com>
2026-04-05 15:46:45 -0600

    room code working

---
+++ js/room_code.js
@@ -0,0 +1,17 @@
+
+window.addEventListener('load', () => {
+    document.getElementById('roomForm').addEventListener('submit', function (e) {
+        e.preventDefault(); // Stop normal form submission
+
+            const code = document.getElementById('roomCode').value.trim();
+
+        if (code) {
+            // const path = "./main_page.html"; // switch this when online and not local
+            const path = "http://127.0.0.1:5000/main_page.html";
+            const targetURL = `${path}?room=${encodeURIComponent(code)}`;
+            window.location.href = targetURL;
+            } else {
+                alert("Please enter a valid room code.");
+            }
+    });
+});
